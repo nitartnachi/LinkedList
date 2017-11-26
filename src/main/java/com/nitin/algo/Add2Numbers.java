e921e07841d5ddfdad6ae0ca709b4713f636099d
Nitin Sharma <nsharma78@gmail.com>
2017-11-26 13:41:48 -0800

    LinkedListUtils implemented

--- src/main/java/com/nitin/algo/Add2Numbers.java
+++ src/main/java/com/nitin/algo/Add2Numbers.java
@@ -1,7 +1,10 @@
 /*
- * You are given two linked lists representing two non-negative numbers. The digits are stored in reverse order and each of their nodes contain a single digit.
+ * You are given two linked lists representing two non-negative numbers. 
+ * The digits are stored in reverse order and each of their nodes contain a single digit.
  * Add the two numbers and return it as a linked list.
- * Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+ * Input: (2 -> 4 -> 3) 
+ *      + (5 -> 6 -> 4)
+ *      
  * Output: 7 -> 0 -> 8
  */
 package com.nitin.algo;
---
+++ src/main/java/com/nitin/algo/LinkedListUtils.java
@@ -0,0 +1,47 @@
+/*
+ * Manage creation, deletion, updates in linked list.
+ */
+
+package com.nitin.algo;
+
+public class LinkedListUtils {
+
+	// Node
+	static class Node{
+		int data;
+		Node next;
+
+		Node(int d){
+			data = d;
+			next = null;
+		}
+	}
+	
+	public static Node createLinkedList(int[] arr) {
+		Node head = new Node(arr[0]);
+		Node curr = head;
+		for(int i = 1; i < arr.length; i++) {
+			Node temp = new Node(arr[i]);
+			curr.next = temp;
+			curr = curr.next;
+		}
+		return head;
+		
+	}
+	
+	public static void printList(Node head) {
+		while(head != null) {
+			System.out.print(head.data + "->");
+			head = head.next;
+		}
+		System.out.println("NULL");
+	}
+	
+	public static void main(String[] args) {
+		int[] arr = {1,2,3,4,5,6,7,8,9};
+		Node head = createLinkedList(arr);
+		printList(head);
+	}
+
+
+}
